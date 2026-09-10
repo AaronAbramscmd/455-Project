d3b08be66e48f17084e91964b2707895c45f58ee
unclesammycat1776-pixel <unclesammycat1776@gmail.com>
2026-09-10 11:37:02 -0400

    test
    testing if it works

---
+++ MSG/MSG/HoursLocations.php
@@ -0,0 +1,79 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hours and Locations</title>
+	<link rel="stylesheet"  href="CSS/HoursPage.css">
+	
+
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li>
+					<a href="MSG_HomePage.php"> 
+						<img src="/Websites/MSG/Image/Logo.gif" alt="logo" width="150" height="150">
+					</a>
+				</li>
+				<li> <a href="MSG_HomePage.php">Home</a> </li>
+				<li> <a href="HoursLocations.php">Hours/Locations</a> </li>
+				<li> <a href="#Field">Paintball Field</a> </li>
+				<li> <a href="#Prices">Prices</a> </li>
+				<li> <a href="#PBirthday">Paintball Birthday Parties</a> </li>
+				<li> <a href="#ABirthday">Airsoft Birthday Parties</a> </li>
+				<li> <a href="#Gell">Gell Ball Parties</a> </li>
+				<li> <a href="#FAQ">FAQ</a> </li>
+				<li> <a href="#Contact">Contact</a> </li>
+				<li> <a href="https://montgomery-sporting-goods-paintball.myshopify.com/"> Online Store</a></li>
+			</ul>
+		</nav>
+	</header>
+<div class="Shop">
+	<h2> Pro-Shop is located at:</h2>
+	<h4>(This is not the Painball field)</h4>
+	<h4> 1934 Route 211 East Middletown, NY 10941</h4>
+	<h4> 845-457-4678 </h4>
+
+	<h2> Shop Hours </h2>
+	<h3> Monday</h3>
+	<h4> 11:00am-5:00pm</h4>
+	<h3> Tuesday </h3>
+	<h4> 11:00am-5:00pm </h4>
+	<h3> Wednesday </h3>
+	<h4> 11:00am-5:00pm </h4>
+	<h3> Thursday </h3>
+	<h4> 11:00am-5:00pm </h4>
+	<h3> Friday </h3>
+	<h4> 11:00am-5:00pm </h4>
+	<h3> Saturday </h3>
+	<h4> 9:00am-5:00pm </h4>
+	<h3> Sunday </h3>
+	<h4> 9:00am-5:00pm </h4>
+
+	<h5> Family owned and operated for 26 years </h5>
+
+	<h4> Largest paintball pro-shop in the Tri-State. <br>Fully stocked for all your paintball needs!</h4>
+	<li>Paintball gun repairs by certified paintball techs</li>
+	<li>Co2 refills up to 24 ounce tanks</li>
+	<li>Compresses air fills up to 4,500PSI</li>
+	<li>Smoke grenades for gender reveals</li>
+	<li>Green gas for airsoft</li>
+	<li>Airsoft BB's</li>
+	<li>Red dot sights, and lasers</li>
+	<li>Home defense</li>
+	<h3>Something for everyone!</h3>
+
+	<h2> Brands we carry in our shop</h2>
+	<p> Dye, Planet Eclipse, Valken, Tippmann, Exalt, <br>HK Army, First Strike, Bunkerkings, Push, Umarex, <br>Elite Force, Empire, JT, VForce, Condor,Social Paintball, <br>Virtue, Gen-X-Global, CP, Inception Designs, <br>Killhouse Weapon Systems, GOG/Luxe</p>
+</div>
+
+
+</body>
+</html>
---
+++ MSG/MSG/MSG_HomePage.php
@@ -0,0 +1,54 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Montgomery Sporting Goods & Paintball - Airsoft, Paintball</title>
+	<link rel="icon" type="Image/png" href="Image/Logo.gif">
+	<link rel="stylesheet" href="CSS/HomePage.css">
+	
+	
+		
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li> <a href="MSG_HomePage.php"> 
+						<img src="/Websites/MSG/Image/Logo.gif" alt="logo" width="150" height="150">
+					</a>
+				</li>
+				<li> <a href="MSG_HomePage.php">Home</a> </li>
+				<li> <a href="HoursLocations.php">Hours/Locations</a> </li>
+				<li> <a href="#Field">Paintball Field</a> </li>
+				<li> <a href="#Prices">Prices</a> </li>
+				<li> <a href="#PBirthday">Paintball Birthday Parties</a> </li>
+				<li> <a href="#ABirthday">Airsoft Birthday Parties</a> </li>
+				<li> <a href="#Gell">Gell Ball Parties</a> </li>
+				<li> <a href="#FAQ">FAQ</a> </li>
+				<li> <a href="#Contact">Contact</a> </li>
+				<li> <a href="https://montgomery-sporting-goods-paintball.myshopify.com/"> Online Store</a></li>
+			</ul>
+		
+		</nav>
+	</header>
+
+		
+  <h1>Montgomery Sporting Goods & Paintball - Airsoft, Paintball</h1>
+
+<h2> Want to book a party?</h2>
+<h3> Click bleow</h3>
+<div class="Book"> 
+	<a href="Book.php"><img src="Image/Book3.png" ></a>
+</div>
+	  
+	
+	
+
+</body>
+</html>
